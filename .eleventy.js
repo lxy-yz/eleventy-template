3deaa51e027295490537e112ee96137e79232a89
lix <lix.dev.yz@gmail.com>
2021-11-20 11:51:16 -0800

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const fs = require("fs")
+const now = String(Date.now())
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./styles/tailwind.config.js")
+  eleventyConfig.addWatchTarget("./styles/tailwind.css")
+
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
+  })
+
+  eleventyConfig.addShortcode("version", function () {
+    return now
+  })
+
+  eleventyConfig.addFilter("json", (value) => {
+    return JSON.stringify(value, null, 2)
+  })
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready(err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content404 = fs.readFileSync("_site/404.html")
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
+          res.write(content404)
+          res.end()
+        })
+      },
+    },
+  })
+}
---
+++ posts/hello-world.md
@@ -0,0 +1,7 @@
+---
+tags: post
+---
+
+# Hello World
+
+This is a hello world post!
